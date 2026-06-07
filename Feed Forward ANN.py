6e7675695ab49294cfb61ee456dae52e8aed4918
Pramita Sarkar <pramitasarkar20@gmail.com>
2026-06-07 22:01:10 +0530

    Update Feed Forward ANN.py

--- Feed Forward ANN.py
+++ Feed Forward ANN.py
@@ -1,30 +1,43 @@
+# Importing modules
 import numpy as np
-X = [[2.5, 5.6, 0.5],
-     [1.4, 2.5, 1.1],
-     [2.3, 4.7, 3.3]]
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
 
 # Initial Framework
 class artificial_neuron:
-    def __init__(self, weights, neurons):
-        self.weights = np.random.rand(weights, neurons)
-        self.bias = np.random.rand(neurons)
-    def forward (self, X):
-        self.output = np.dot(X, self.weights)+self.bias
+    def __init__(self, inputs, neurons):
+        self.weights = 0.01 * np.random.rand(inputs, neurons)
+        self.bias = np.random.rand(1, neurons)
+    def forward (self, inputs):
+        self.output = np.dot(inputs, self.weights)+self.bias
 
 # Rectified Linear Activation Function
 class activation_ReLU:
-    def forward (self, X):
-        self.output = np.maximum(0,X)
+    def forward(self, inputs):
+        self.output = np.maximum(0,inputs)
+
+# Softmax Activation Function
+class activation_Softmax:
+    def forward(self, inputs):
+        exponential = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exponential/np.sum(exponential, axis=1, keepdims=True)
+        self.output = probabilities
+
+X , y = spiral_data(samples=100, classes=3)
 
 # Assigning Layers
-layer1 = artificial_neuron(3,7)
-layer2 = artificial_neuron(7,2)
-activation = activation_ReLU()
+layer1 = artificial_neuron(2,9)
+activation1 = activation_ReLU()
+
+layer2 = artificial_neuron(9,3)
+activation2 = activation_Softmax()
 
 # Relating to each other
 layer1.forward(X)
-activation.forward(layer1.output)
-layer2.forward(activation.output)
+activation1.forward(layer1.output)
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
 
 # Output
-print(layer2.output)
+print(activation2.output)
