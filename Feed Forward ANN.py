53ebb4ed0b82732273a3fc6ab722b7966e754a3a
Pramita Sarkar <pramitasarkar20@gmail.com>
2026-06-09 12:58:55 +0530

    Update Feed Forward ANN.py

--- Feed Forward ANN.py
+++ Feed Forward ANN.py
@@ -8,7 +8,7 @@ nnfs.init()
 class artificial_neuron:
     def __init__(self, inputs, neurons):
         self.weights = 0.01 * np.random.rand(inputs, neurons)
-        self.bias = np.random.rand(1, neurons)
+        self.bias = np.zeros((1, neurons))
     def forward (self, inputs):
         self.output = np.dot(inputs, self.weights)+self.bias
 
@@ -23,6 +23,27 @@ class activation_Softmax:
         exponential = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exponential/np.sum(exponential, axis=1, keepdims=True)
         self.output = probabilities
+
+# Calculating Loss with Cross-Categorical Entropy
+class Loss:
+    def calculate(self, output, y): # here 'y' is target values
+        sample_loss = self.forward(output,y)
+        data_loss = np.mean(sample_loss)
+        return data_loss
+
+class CrossCategoricalEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        sample = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            confidence = y_pred_clipped[range(sample), y_true] 
+        elif len(y_true.shape) == 2:
+            confidence = np.sum(y_pred_clipped * y_true ,axis =1)
+
+        negative_log = -np.log(confidence)
+        return negative_log
+    
 
 X , y = spiral_data(samples=100, classes=3)
 
@@ -41,3 +62,8 @@ activation2.forward(layer2.output)
 
 # Output
 print(activation2.output)
+
+# Calculating Loss
+loss_function = CrossCategoricalEntropy()
+loss = loss_function.calculate(activation2.output,y)
+print('Loss:', loss)
